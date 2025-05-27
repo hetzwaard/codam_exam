4cc87f0bd411c78a9badeda84bd556a1920a5a19
Mahmut Kilic <71298914+hetzwaard@users.noreply.github.com>
2025-05-27 16:02:40 +0200

    Update fprime.c

--- fprime/fprime.c
+++ fprime/fprime.c
@@ -1,33 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void	fprime(int nb)
+void	fprime(unsigned int number)
 {
 	unsigned int	prime;
-
-	if (nb == 1)
-		printf("1\n");
-	prime = 2;
-	while (nb > 1)
+	if (number == 1)
+		printf("1");
+	else
 	{
-		if (nb % prime == 0)
+		prime = 2;
+		while (number > 1)
 		{
-			printf("%d", prime);
-			nb /= prime;
-			if (nb > 1)
-				printf("*");
-			prime--;
+			if (number % prime == 0)
+			{
+				printf("%d", prime);
+				number /= prime;
+				if (number > 1)
+					printf("*");
+				prime--;
+			}
+			prime++;
 		}
-		prime++;
 	}
-	printf("\n");
 }
 
-int	main(int ac, char **av)
+int main (int ac, char **av)
 {
-	if (ac == 2 && *av[1])
-		fprime(atoi(av[1]));
+	if (ac != 2)
+		return (printf("\n"), 0);
+	else if (*av[1] && av[1][0] == '-')
+		return (printf("\n"), 0);
 	else
-		printf("\n");
+		fprime(atoi(av[1]));
+	printf("\n");
 	return (0);
 }
