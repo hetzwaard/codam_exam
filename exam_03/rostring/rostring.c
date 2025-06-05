555e48f6baeef33c466ba09ab10f8a91dd98fbb5
Mahmut Kilic <mahkilic@f1r6s18.codam.nl>
2025-06-05 15:12:27 +0200

    update

--- exam_03/rostring/rostring.c
+++ exam_03/rostring/rostring.c
@@ -1,36 +1,44 @@
 #include <unistd.h>
-#include <stdlib.h>
 
-// get the first word.
-// print it at the end.
-// fwi is now the end index of the first word.
-
-// so my plan was that I would take the first word's index, (first and the last index)
-// after getting those indexes, we could pass the word and start writing the string with 
-// taking spaces into account. only one space between the words. we need take this into account.
-// after writing a word, we pass all the spaces, and if we don't get null terminator, we just print " ".
-// that plan seems like could work. Easy peasy.
+// first, take the first and the last index of the first word
+// then start printing from the first index of the second word
+// pass the white spaces and there should be only one space between words
+// at the end, print the first word.
 
 int	main(int ac, char **av)
 {
 	if (ac < 2)
-		return (write(1, "\n", 1), 0);
+		return(write(1, "\n", 1), 0);
 
 	int i = 0;
 	int fw_f = 0;
 	int fw_l = 0;
+	int flag = 0;
 	char *str = av[1];
-	
+
 	while (str[i] && (str[i] == ' ' || str[i] == '\t'))
 		i++;
 	fw_f = i;
-	while (str[i] && (str[i] != 9 || str[i] != 10 || str[i] != 11 || str[i] != 12 || str[i] != 13 || str[i] != 32))
+	while (str[i] && (str[i] != ' ' || str[i] == '\t'))
 		i++;
 	fw_l = i;
-	while (str[fw_f] != str[fw_l])
+	while (str[i] && (str[i] == ' ' || str[i] == '\t'))
+		i++;
+	while (str[i])
 	{
-		write(1, &str[fw_f], 1);
-		fw_f++;
+		if (str[i] == ' ' || str[i] == '\t')
+		{
+			while (str[i] && (str[i] == ' ' || str[i] == '\t'))
+				i++;
+			if (str[i] != ' ' || str[i] != '\t' || str[i] != '\0')
+				write(1, " ", 1);
+		}
+		flag = 1;
+		write(1, &str[i++], 1);
 	}
-	return (write(1, "\n", 1), 0);
+	if (flag == 1)
+		write(1, " ", 1);
+	while (str[fw_f] != str[fw_l])
+		write(1, &str[fw_f++], 1);
+	return(write(1, "\n", 1), 0);
 }
