4158a6315efb43f30cfb8a8ab3a7c8f19952cc8b
Mahmut Kilic <mahmutsedatkilic@gmail.com>
2025-11-07 18:13:24 +0100

    get_next_line update and remove binary_search

--- exam_03/binary_search/binary_search.c
+++
@@ -1,8 +0,0 @@
-#include "binary_search.h"
-
-int	size_binary_tree(t_btree *root)
-{
-	if (!root)
-		return (0);
-	return (1 + size_binary_tree(root->left) + size_binary_tree(root->right));
-}
--- exam_03/get_next_line/get_next_line.c
+++ exam_03/get_next_line/get_next_line.c
@@ -9,24 +9,33 @@
 
 char	*get_next_line(int fd)
 {
-	(void)BUFFER_SIZE;
-	char	*s = malloc(1000000), *c = s;
-	while (read(fd, c, 1) > 0 && *c++ != '\n');
+	char *s = malloc(1000000), *c = s;
+	int i = 0;
+	while (BUFFER_SIZE >= i && *c != '\n')
+	{
+		read(fd, c, BUFFER_SIZE);
+		if (*c == '\0')
+		{
+			i++;
+			break ;
+		}
+		c += BUFFER_SIZE;
+	}
 	return c > s ? (*c = 0, s) : (free(s), NULL);
 }
 
-int main(void)
-{
-	char *line;
-	int fd = open("text.txt", O_RDONLY);
-	while (1)
-	{
-		line = get_next_line(fd);
-		if (!line)
-			break ;
-		printf("%s", line);
-		free(line);
-	}
-	close(fd);
-	return (0);
-}
+// int main(void)
+// {
+// 	char *line;
+// 	int fd = open("text.txt", O_RDONLY);
+// 	while (1)
+// 	{
+// 		line = get_next_line(fd);
+// 		if (!line)
+// 			break ;
+// 		printf("%s", line);
+// 		free(line);
+// 	}
+// 	close(fd);
+// 	return (0);
+// }
