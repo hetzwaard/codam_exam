f32b143730b11d7bec1f75ed557ae5c92e9c3887
Mahmut Kilic <mahmutsedatkilic@gmail.com>
2025-11-22 19:14:39 +0100

    add ft_popen

---
+++ rank04/0/ft_popen/ft_popen.c
@@ -0,0 +1,50 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+int	ft_popen(const char *file, char *const argv[], char type)
+{
+	int	fd[2];
+	pid_t	pid;
+
+	if (!file || !argv || (type != 'r' && type != 'w'))
+		return (-1);
+	if (pipe(fd) == -1)
+		return (-1);
+	pid = fork();
+	if (pid == -1)
+	{
+		close(fd[0]);
+		close(fd[1]);
+		return (-1);
+	}
+	if (pid == 0)
+	{
+		if (type == 'r')
+		{
+			close(fd[0]);
+			if (dup2(fd[1], STDOUT_FILENO) == -1)
+				exit(1);
+			close(fd[1]);
+		}
+		else
+		{
+			close(fd[1]);
+			if (dup2(fd[0], STDOUT_FILENO) == -1)
+				exit(1);
+			close(fd[0]);
+		}
+		execvp(file, argv);
+		exit(1);
+	}
+	if (type == 'r')
+	{
+		close(fd[1]);
+		return (fd[0]);
+	}
+	else
+	{
+		close(fd[0]);
+		return (fd[1]);
+	}
+}
